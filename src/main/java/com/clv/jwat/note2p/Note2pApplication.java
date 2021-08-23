76f0ca25706934d1598ec1073ff0ac73d3128fed
huyphuoc206 <huyphuoc206@gmail.com>
2021-08-23 21:35:12 +0700

    Init api project

---
+++ src/main/java/com/clv/jwat/note2p/Note2pApplication.java
@@ -0,0 +1,13 @@
+package com.clv.jwat.note2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Note2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Note2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clv/jwat/note2p/Note2pApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clv.jwat.note2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Note2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
